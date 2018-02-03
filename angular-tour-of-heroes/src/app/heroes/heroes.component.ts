07f2e3679591776b971ab5efd5e0e8e9b5e251d1
Abhishek Gopalan <abhi4717@gmail.com>
2018-02-03 04:54:29 -0800

    Completed angular.io tutorial

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -17,13 +16,6 @@ export class HeroesComponent implements OnInit {
   }
   heroes : Hero[];
 
-	selectedHero: Hero;
-
-	onSelect(hero: Hero): void{
-
-		this.selectedHero = hero;
-	}
-	
 	getHeroes() : void{
 	this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);	
 	}
